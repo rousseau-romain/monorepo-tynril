f47b79bf7bf1d51c187f985bd150c8e70ee39602
rousseau-romain <rousseau.romain82@gmail.com>
2023-04-30 00:48:15 +0200

    eslint working

--- apps/web/src/routes/monster/[id]/+page.server.ts
+++ apps/web/src/routes/monster/[id]/+page.server.ts
@@ -1,15 +1,15 @@
 import { error } from '@sveltejs/kit'
 import type { PageServerLoad } from './$types'
 
-import { PrismaClient } from 'database'
+import { PrismaClient } from "database";
 
 export const load = (async ({ params }) => {
-  const prisma = new PrismaClient()
+  const prisma  = new PrismaClient()
   const monster = await prisma.monster.findFirst({
     where: {
       idDofus: Number(params.id),   
     },
-  })     
+  })       
 
   if (monster) {
     return { monster }
